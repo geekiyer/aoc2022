326e1e49a3955c357d5c00c40cb571befa776768
Gopalakrishnan Iyer <gopalakrishnan.iyer@aexp.com>
2022-12-04 23:26:27 -0700

    Day 5

---
+++ src/main/java/Day5.kt
@@ -0,0 +1,30 @@
+fun main() {
+    println(solve(false))
+    println(solve(true))
+}
+
+val rearrangementInput = object {}.javaClass.getResourceAsStream("day5.txt")?.bufferedReader()?.readLines()!!
+
+fun solve(partTwo: Boolean): String {
+    val crates = (1..9).associateWith { emptyList<Char>() }.toMutableMap()
+    rearrangementInput.filter { '[' in it }.forEach { line ->
+        for (stackIndex in 1..9) {
+            val lineIndex = 1 + (stackIndex - 1) * 4
+            if (line.lastIndex < lineIndex || line[lineIndex] == ' ') continue
+            crates[stackIndex] = listOf(line[lineIndex]) + crates.getValue(stackIndex)
+        }
+    }
+    rearrangementInput.filter { "move" in it }.forEach { line ->
+        val (amount, from, to) = "\\d+".toRegex().findAll(line).map { it.value.toInt() }.toList()
+        if (partTwo) {
+            crates[to] = crates.getValue(to) + crates.getValue(from).takeLast(amount)
+            crates[from] = crates.getValue(from).dropLast(amount)
+        } else {
+            repeat(amount) {
+                crates[to] = crates.getValue(to) + crates.getValue(from).last()
+                crates[from] = crates.getValue(from).dropLast(1)
+            }
+        }
+    }
+    return crates.values.map { it.last() }.joinToString("")
+}
